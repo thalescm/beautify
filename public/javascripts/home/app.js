745cddef42caf60ca91eb136a80b422d03a916af
Thales Machado <thales.machado@taqtile.com>
2014-05-13 23:59:17 -0300

    Added basic navigation

--- public/javascripts/home/app.js
+++ public/javascripts/home/app.js
@@ -97,58 +97,7 @@ console.log(PSD);
 
 PSD["BackIcon"].visible = false;
 
-PSD["AjustesGray"].visible = true;
-PSD["AjustesPink"].visible = false;
 
-PSD["AgendarPink"].visible = true;
-PSD["AgendarGray"].visible = false;
-
-PSD["HistoricoPink"].visible = false;
-PSD["HistoricoGray"].visible = true;
-
-PSD["List scroll"].visible = true;
-
-PSD["AjustesGray"].on("click", function(ev) {
-
-	PSD["AjustesGray"].visible = false;
-	PSD["AjustesPink"].visible = true;
-
-	PSD["AgendarPink"].visible = false;
-	PSD["AgendarGray"].visible = true;
-
-	PSD["HistoricoPink"].visible = false;
-	PSD["HistoricoGray"].visible = true;
-
-	PSD["List scroll"].visible = false;
-});
-
-PSD["AgendarGray"].on("click", function(ev) {
-
-	PSD["AjustesGray"].visible = true;
-	PSD["AjustesPink"].visible = false;
-
-	PSD["AgendarPink"].visible = true;
-	PSD["AgendarGray"].visible = false;
-
-	PSD["HistoricoPink"].visible = false;
-	PSD["HistoricoGray"].visible = true;
-
-	PSD["List scroll"].visible = true;
-});
-
-PSD["HistoricoGray"].on("click", function(ev) {
-
-	PSD["AjustesGray"].visible = true;
-	PSD["AjustesPink"].visible = false;
-
-	PSD["AgendarPink"].visible = false;
-	PSD["AgendarGray"].visible = true;
-
-	PSD["HistoricoPink"].visible = true;
-	PSD["HistoricoGray"].visible = false;
-
-	PSD["List scroll"].visible = false;
-});
 
 
 // PSD["List"].on("click", function(ev) {
